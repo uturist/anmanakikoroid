53bacec8f3695d7438562842e8f16aa2df7db805
uturist <sato_eiki@yahoo.co.jp>
2014-04-05 14:18:57 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
-<html lang="ja">
-<head>
-<meta http-equiv="Content-Type" 
-        content="text/html; charset=utf-8">
-<title>あんまんあきこロイドちゃん</title>
-</head>
 
-<body>
-<p>あんまんあきこロイドちゃんのページです。</p>
 <?php
 $text = fopen("text.txt", "r");
 
@@ -27,5 +19,3 @@ if ($text){
 fclose($text);
 
 ?>
-</body>
-</html>

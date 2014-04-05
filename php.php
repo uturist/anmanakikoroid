11920008c7b87a9940a8af61ef207eb6c2783dea
uturist <sato_eiki@yahoo.co.jp>
2014-04-05 13:06:47 +0900

    Update php.php

--- php.php
+++ php.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
 <meta http-equiv="Content-Type" 
-        content="text/html; charset=Shift_Jis">
+        content="text/html; charset=utf-8">
 <title>PHPテスト</title>
 </head>
 

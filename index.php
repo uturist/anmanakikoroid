8e615f595ed5dd70ac19957b3a7f6b7f90ef2a24
uturist <sato_eiki@yahoo.co.jp>
2014-04-05 13:21:31 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" 
+        content="text/html; charset=utf-8">
+<title>あんまんあきこロイドちゃん</title>
+</head>
+
+<body>
+<p>あんまんあきこロイドちゃんのページです。</p>
+</body>
+</html>
